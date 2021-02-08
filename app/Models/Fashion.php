d9025c4bd87f3eedabba138d054815872764d24a
divya <divyapatange0@gmail.com>
2021-02-08 19:16:28 +0530

    fashion table

---
+++ app/Models/Fashion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fashion extends Model
+{
+    use HasFactory;
+
+    protected $table = "fashions";
+
+    protected $fillable = ['user_id', 'sub_category_id','type_id', 'size','condition', 'age',
+    'ad_title', 'description', 'photos', 'price', 'name', 'email', 'mobile_no','state_id', 'city_id', 'locality_id',
+    'pin_code', 'address', 'user_type', 'gst_no'];
+}
